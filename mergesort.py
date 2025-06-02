bebd1b6fcd4f8a7b82b2ba296f760aaeaecdd7da
Batoul Mhawesh <batoul.mhawesh01@stud.uni-goettingen.de>
2025-06-02 16:02:56 +0200

    fix redundant if statement

--- mergesort.py
+++ mergesort.py
@@ -1,40 +1,37 @@
 import matplotlib.pyplot as plt
 
 def mergeSort(list):
-    if (
-        len(list) > 1
-        and not len(list) < 1
-        and len(list) != 0
-    ):
-        mid = len(list) // 2
-        left = list[:mid]
-        right = list[mid:]
+    if len(list) <= 1:
+        return  # Basisfall: Liste ist bereits sortiert
+    mid = len(list) // 2
+    left = list[:mid]
+    right = list[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+    mergeSort(left)
+    mergeSort(right)
 
-        left_index = 0
-        right_index = 0
-        i = 0
+    left_index = 0
+    right_index = 0
+    i = 0
 
-        while left_index < len(left) and right_index < len(right):
-            if left[left_index] <= right[right_index]:
-                list[i] = left[left_index]
-                left_index += 1
-            else:
-                list[i] = right[right_index]
-                right_index += 1
-            i += 1
-
-        while left_index < len(left):
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
             list[i] = left[left_index]
             left_index += 1
-            i += 1
-
-        while right_index < len(right):
+        else:
             list[i] = right[right_index]
             right_index += 1
-            i += 1
+        i += 1
+
+    while left_index < len(left):
+        list[i] = left[left_index]
+        left_index += 1
+        i += 1
+
+    while right_index < len(right):
+        list[i] = right[right_index]
+        right_index += 1
+        i += 1    
 
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
