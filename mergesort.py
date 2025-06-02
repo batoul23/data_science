91865195ed4251005c18630fa3915a9c5610b7df
Batoul Mhawesh <batoul.mhawesh01@stud.uni-goettingen.de>
2025-06-02 15:43:44 +0200

    change big variable names

--- mergesort.py
+++ mergesort.py
@@ -2,15 +2,15 @@ def ASSIGNMENT(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
 
-def mergeSort(list_to_sort_by_merge):
+def mergeSort(list):
     if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
+        len(list) > 1
+        and not len(list) < 1
+        and len(list) != 0
     ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+        mid = len(list) // 2
+        left = list[:mid]
+        right = list[mid:]
 
         mergeSort(left)
         mergeSort(right)
@@ -21,20 +21,20 @@ def mergeSort(list_to_sort_by_merge):
 
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                ASSIGNMENT(new_list=list, i=i, old_list=left, j=l)
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                ASSIGNMENT(new_list=list, i=i, old_list=right, j=r)
                 r += 1
             i += 1
 
         while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
+            list[i] = left[l]
             l += 1
             i += 1
 
         while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
+            list[i] = right[r]
             r += 1
             i += 1
 
